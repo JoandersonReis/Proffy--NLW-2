8bbf0d8a78ba10f276a7bc2bc0c16252600d7f4e
Joanderson Reis <joandersonreis470@gmail.com>
2021-08-03 20:16:04 -0300

    Finished Restructure Landing Page

--- web/src/components/Finished/index.tsx
+++ web/src/components/Finished/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import successCheckIcon from "../../assets/images/icons/success-check-icon.svg"
+
 import "./styles.css"
 
 interface FinishedProps {
@@ -14,6 +16,7 @@ const Finished: React.FC<FinishedProps> = ({ title, description, buttonText, rou
   return (
     <div id="finished-container">
       <main>
+        <img src={successCheckIcon} alt="Icone de checagem" />
         <h1>{title}</h1>
 
         <p>{description}</p>
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -10,6 +10,7 @@ import landingImg from "../../assets/images/landing.svg"
 import studyIcon from "../../assets/images/icons/study.svg"
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg"
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg"
+import logoutIcon from "../../assets/images/icons/logout.svg"
 
 
 function Landing() {
@@ -26,33 +27,48 @@ function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online.</h2>
-        </div>
+        <main>
+          <header>
+            <span className="profile">
+              <img src="https://avatars.githubusercontent.com/u/52385035?v=4" alt="Imagem de perfil" />
+              Joanderson Reis
+            </span>
 
-        <img 
-          src={landingImg} 
-          alt="Plataforma de estudos" 
-          className="hero-image" 
-        />
+            <button type="button"><img src={logoutIcon} alt="Botão de sair" /></button>
+          </header>
 
-        <div className="buttons-container">
-          <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar" />
-            Estudar
-          </Link>
+          <div className="logo-container">
+            <img src={logoImg} alt="Proffy" />
+            <h2>Sua plataforma de estudos online.</h2>
+          </div>
 
-          <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar Aulas" />
-            Dar Aulas
-          </Link>
-        </div>
+          <img 
+            src={landingImg} 
+            alt="Plataforma de estudos" 
+            className="hero-image" 
+          />
+        </main>
 
-        <span className="total-connections">
-          Total de {totalConnections} conexões ja realizadas 
-          <img src={purpleHeartIcon} alt="Coração roxo" />
-        </span>
+        <footer>
+          <p className="apresentation">Seja bem vindo. <br/> <span>O que deseja fazer?</span></p>
+
+          <div className="buttons-container">
+            <Link to="/study" className="study">
+              <img src={studyIcon} alt="Estudar" />
+              Estudar
+            </Link>
+
+            <Link to="/give-classes" className="give-classes">
+              <img src={giveClassesIcon} alt="Dar Aulas" />
+              Dar Aulas
+            </Link>
+          </div>
+
+          <span className="total-connections">
+            Total de {totalConnections} conexões ja realizadas 
+            <img src={purpleHeartIcon} alt="Coração roxo" />
+          </span>
+        </footer>
       </div>
     </div>
   )
